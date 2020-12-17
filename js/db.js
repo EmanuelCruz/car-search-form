5547e28c289974963c857e2c67bf78eafa6d8eff
EmanuelCruz <emanuelcruz091@gmail.com>
2020-12-17 12:23:12 -0300

    Base de datos de práctica

---
+++ js/db.js
@@ -0,0 +1,192 @@
+export const autos = [
+	{
+		brand: 'BMW',
+		model: 'Serie 3',
+		year: 2020,
+		price: 30000,
+		doors: 4,
+		colour: 'Blanco',
+		transmission: 'Automatico',
+		img: "./assets/img/bmw-serie-3-2020-blanco.jpg"
+	},
+	{
+		brand: 'Audi',
+		model: 'A4',
+		year: 2020,
+		price: 40000,
+		doors: 4,
+		colour: 'Negro',
+		transmission: 'Automatico',
+		img: "./assets/img/audi-a4-2020-negro.jpg"
+	},
+	{
+		brand: 'Ford',
+		model: 'Mustang',
+		year: 2015,
+		price: 20000,
+		doors: 2,
+		colour: 'Blanco',
+		transmission: 'Automatico',
+		img: "./assets/img/ford-mustang-2015-blanco.jpg"
+	},
+	{
+		brand: 'Audi',
+		model: 'A6',
+		year: 2020,
+		price: 35000,
+		doors: 4,
+		colour: 'Negro',
+		transmission: 'Automatico',
+		img: "./assets/img/audi-a6-2020-negro.jpg"
+    },
+	{
+		brand: 'BMW',
+		model: 'Serie 5',
+		year: 2017,
+		price: 70000,
+		doors: 4,
+		colour: 'Negro',
+		transmission: 'Automatico',
+		img: "./assets/img/bmw-serie-5-2017-negro-4.jpg"
+	},
+	{
+		brand: 'Mercedes Benz',
+		model: 'Clase C',
+		year: 2015,
+		price: 25000,
+		doors: 4,
+		colour: 'Blanco',
+		transmission: 'Automatico',
+		img: "./assets/img/mercedes-benz-clase-c-2015-blanco.jpg"
+	},
+	{
+		brand: 'Chevrolet',
+		model: 'Camaro',
+		year: 2018,
+		price: 60000,
+		doors: 2,
+		colour: 'Rojo',
+		transmission: 'Manual',
+		img: "./assets/img/chevrolet-camaro-2018-rojo.png"
+	},
+	{
+		brand: 'Ford',
+		model: 'Mustang',
+		year: 2019,
+		price: 80000,
+		doors: 2,
+		colour: 'Rojo',
+		transmission: 'Manual',
+		img: "./assets/img/ford-mustang-2019-rojo.jpg"
+	},
+	{
+		brand: 'Dodge',
+		model: 'Challenger',
+		year: 2020,
+		price: 40000,
+		doors: 4,
+		colour: 'Blanco',
+		transmission: 'Automatico',
+		img: "./assets/img/dodge-challenger-2020-blanco.jpg"
+	},
+	{
+		brand: 'Audi',
+		model: 'A3',
+		year: 2017,
+		price: 55000,
+		doors: 2,
+		colour: 'Negro',
+		transmission: 'Manual',
+		img: "./assets/img/audi-a3-2017-negro.jpg"
+    },
+	{
+		brand: 'Dodge',
+		model: 'Challenger',
+		year: 2020,
+		price: 25000,
+		doors: 2,
+		colour: 'Rojo',
+		transmission: 'Manual',
+		img: "./assets/img/dodge-challenger-2020-rojo.jpg"
+	},
+	{
+		brand: 'Mercedes Benz',
+		model: 'Clase C',
+		year: 2018,
+		price: 45000,
+		doors: 4,
+		colour: 'Azul',
+		transmission: 'Automatico',
+		img: "./assets/img/mercedes-benz-clase-c-2018-azul.jpg"
+	},
+	{
+		brand: 'BMW',
+		model: 'Serie 5',
+		year: 2019,
+		price: 90000,
+		doors: 4,
+		colour: 'Blanco',
+		transmission: 'Automatico',
+		img: "./assets/img/bmw-serie-5-2019-blanco.jpg"
+	},
+	{
+		brand: 'Ford',
+		model: 'Mustang',
+		year: 2017,
+		price: 60000,
+		doors: 2,
+		colour: 'Negro',
+		transmission: 'Manual',
+		img: "./assets/img/ford-mustang-2017-negro.jpg"
+    },
+	{
+		brand: 'Dodge',
+		model: 'Challenger',
+		year: 2015,
+		price: 35000,
+		doors: 2,
+		colour: 'Azul',
+		transmission: 'Automatico',
+		img: "./assets/img/dodge-challenger-2015-azul.jpg"
+	},
+	{
+		brand: 'BMW',
+		model: 'Serie 3',
+		year: 2018,
+		price: 50000,
+		doors: 4,
+		colour: 'Blanco',
+		transmission: 'Automatico',
+		img: "./assets/img/bmw-serie-3-2018-blanco.jpg"
+	},
+	{
+		brand: 'BMW',
+		model: 'Serie 5',
+		year: 2017,
+		price: 80000,
+		doors: 4,
+		colour: 'Negro',
+		transmission: 'Automatico',
+		img: "./assets/img/bmw-serie-5-2017-negro.jpg"
+	},
+	{
+		brand: 'Mercedes Benz',
+		model: 'Clase C',
+		year: 2018,
+		price: 40000,
+		doors: 4,
+		colour: 'Blanco',
+		transmission: 'Automatico',
+		img: "./assets/img/mercedes-benz-clase-c-2018-blanco.jpg"
+	},
+	{
+		brand: 'Audi',
+		model: 'A4',
+		year: 2016,
+		price: 30000,
+		doors: 4,
+		colour: 'Azul',
+		transmission: 'Automatico',
+		img: "./assets/img/audi-a4-2016-azul.jpg"
+	}
+];
